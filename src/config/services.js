1067c7ee9721b9e12a4c7395dbd52a7c1a228663
Bruno Hideki <bruno.hideki@t2software.com.br>
2020-05-02 21:23:36 -0300

    Mini project done

--- src/config/services.js
+++
@@ -1,7 +0,0 @@
-export const jwt = require('jsonwebtoken')
-export const TOKEN_KEY = "jwt_access_token"
-export const PUBLIC_KEY = require('./public_key.pem')
-export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null
-export const getToken = () => localStorage.getItem(TOKEN_KEY)
-export const login = token => localStorage.setItem(TOKEN_KEY, token)
-export const logout = () => localStorage.removeItem(TOKEN_KEY)
