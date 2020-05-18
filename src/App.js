edb81f10b7cd626792f56a5ca2657d3c15d8eb82
Bruno Hideki <bruno.hideki@t2software.com.br>
2020-05-18 10:35:23 -0300

    folder structure adjust

--- src/App.js
+++ src/App.js
@@ -1,10 +1,92 @@
-import React, { useEffect } from 'react'
-import Pages from './pages'
+import React, { useState } from 'react'
+import { Input } from 'antd'
 
-export default function App() {
+import './index.css'
 
+export default function Index() {
 
+  let [ input, setInput ] = useState({
+      tl: '',
+      tr: '',
+      bl: '',
+      br: ''
+  })
 
-  return Pages
+  function handleChange(e) {
+      const { name, value } = e.target
+
+      setInput(inputs => ({
+          ...inputs,
+          [name]: value
+      }))
+  }
+  
+  return (
+    <div className='main'>
+        <div className='title-wrapper'>
+            <h2 className='title'>
+                Border Radius
+            </h2>
+            <p className='by'>
+                by bhideki
+            </p>
+        </div>
+        <div className='container-wrapper'>
+
+            <div className='container'>
+                <div 
+                    className='content' 
+                    style={{
+                        borderTopLeftRadius: `${input.tl}px `, 
+                        borderTopRightRadius: `${input.tr}px`,
+                        borderBottomLeftRadius: `${input.bl}px`,
+                        borderBottomRightRadius: `${input.br}px`
+                    }}
+                >
+                    <div className='text-br'>
+                        {input.tl && `border-top-left-radius: ${input.tl}px`}
+                        <br/>
+                        {input.tr && `border-top-right-radius: ${input.tr}px`}
+                        <br/>
+                        {input.bl && `border-bottom-left-radius: ${input.bl}px`}
+                        <br/>
+                        {input.br && `border-bottom-right-radius: ${input.br}px`}
+                    </div>
+                </div>
+                <div className='box-tl'>
+                    <Input 
+                        type='number'
+                        name='tl'
+                        value={input['tl']}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='box-tr'>
+                    <Input 
+                        type='number'
+                        name='tr'
+                        value={input['tr']}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='box-bl'>
+                    <Input 
+                        type='number'
+                        name='bl'
+                        value={input['bl']}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='box-br'>
+                    <Input 
+                        type='number'
+                        name='br'
+                        value={input['br']}
+                        onChange={handleChange}
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
 }
-
--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-
 import './index.css'
 import App from './App'
-import Teste from './pages'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
         <Switch>
             <Route path="/" exact={true} component={App} />
-            <Route path="/teste" component={Teste} />
         </Switch>
     </ BrowserRouter>
   </React.StrictMode>,
